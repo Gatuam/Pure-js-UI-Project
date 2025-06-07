51a38dc31c8e7898b1c88a507115d0108f0b424d
ek-gautham <sggtm12@gmail.com>
2025-06-07 00:41:16 -0400

    finishing the pag

--- app.js
+++ app.js
@@ -3,3 +3,27 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true
 });
+
+const elecontainer = document.querySelector('.elecontainer');
+const fixedImg = document.querySelector('.fixed-img');
+
+elecontainer.addEventListener('mouseenter', ()=>{
+  fixedImg.style.display = "block"
+});
+elecontainer.addEventListener('mouseleave', ()=>{
+  fixedImg.style.display = "none"
+});
+
+const elements = document.querySelectorAll('.ele1');
+elements.forEach((ele)=>{
+  ele.addEventListener('mouseenter', ()=>{
+   const images=  ele.dataset.image
+   fixedImg.style.backgroundImage = `url(${images})`
+  });
+});
+
+let swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 50,
+    });
